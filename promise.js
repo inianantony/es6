0ad8fed8e091e8891dcd9720241859598c282174
Inian Antony <inianantony.s@gmail.com>
2020-01-14 08:08:29 +0800

    add promise.all

--- promise.js
+++ promise.js
@@ -1,25 +1,43 @@
-var page = (function() {
+var page = (function () {
 
     var makePromise = function () {
         //States : pending, fulfilled, rejected
-        var p = new Promise(function(resolve, reject) {
-            setTimeout(function() {
-                    var rand = Math.random();
-                    if (rand > 0.5) {
-                        resolve('resolved');
-                    } else {
-                        reject('rejected');
-                    }
-                },
+        var p = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                var rand = Math.random();
+                if (rand > 0.5) {
+                    resolve('p resolved');
+                } else {
+                    reject('p rejected');
+                }
+            },
                 2000);
         });
 
-        p.then(function(value) {
-            alert(value);
+        p.then(function (value) {
+            console.log(value);
         }).catch(function (value) {
-            alert(value);
-        }).finally(function() {
+            console.log(value);
+        }).finally(function () {
             console.log("Set loading to dismiss in finally");
+        });
+
+        var p1 = new Promise(function (resolve, reject) {
+            setTimeout(function () {
+                var rand = Math.random();
+                if (rand > 0.5) {
+                    resolve('p1 resolved');
+                } else {
+                    reject('p1 rejected');
+                }
+            },
+                2000);
+        });
+
+        Promise.all([p, p1]).then(function (values) {
+            console.log("Promise.all completed");
+            console.log(values);
+
         });
     };
 
